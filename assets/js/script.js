929f3ea614d72952d53da3b6543f30c85f1d21d7
Andrew Keiser <webdev410@gmail.com>
2021-06-25 13:10:46 -0700

    got the colors working and save/get local storage are working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,65 @@
-var dateEl = document.getElementById("currentDay"); // for header
+$(document).ready(function () {
+    
 
-currentDay();
-colorCode();
+    // get items from localStorage
+    for (var i = 9; i < 18; i++) {
+        var id = "hr" + i;
+        var description = localStorage.getItem(id);
+        $("#" + id).val(description);
+    }
 
+    // save button
 
-// get from local storage
+    $(".saveBtn").click(function (event) {
+        event.preventDefault();
+        var hourId = $(this).attr("name");
+        var savedItem = "#" + hourId;
+        var userInput = $(savedItem).val();
+        localStorage.setItem(hourId, userInput);
+    });
 
-
-
-// Current Day Function
-
-function currentDay() {
+    // clock
     setInterval(function () {
+        var dateEl = document.getElementById("currentDay"); // for header
         dateEl.textContent = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
     }, 1000);
-}
 
-function colorCode() {
-    // for loop to go through the boxes
-    for (i = 9; i <= 17; i++) {
-        var currentTime = moment().hour(); // current time rounded to the hour
-        var id = "Hour " + i; // for save function
+    // color code
+    function colorCode() {
+        // for loop to go through the boxes
+        for (i = 9; i <= 17; i++) {
+            var currentTime = moment().hour(); // current time rounded to the hour
+            var id = "hr " + i; // for save function
+            
+            if (i === currentTime) {
+                console.log('present');
+                $("#hour" + i).addClass("present");
+                $("#hr" + i).addClass("present");
+                // $(".row").removeClass("past future");
+                
 
-        if (i === currentTime) {
-            console.log('present');
-            $(".row").addClass("time-block present");
+            }
+             else if (i < currentTime) {
+                console.log('past');
+                $("#hour" + i).addClass("past");
+                $("#hr" + i).addClass("past");
+                // $(".row").removeClass("present future");
+              
+            }
+            else {
+                console.log('future');
+                $("#hour" + i).addClass("future");
+                $("#hr" + i).addClass("future");
+            //    $(".row").removeClass("past present");
+            
+            }
+
+            console.log("i: ", i)
+            console.log("Current Time: ", currentTime)
+            console.log("id: ", id)
+            console.log("---------------------------")
         }
-        else if (i < currentTime) {
-            console.log('past');
-            $(".row").addClass("time-block past");
-          
-        }
-        else {
-            console.log('future');
-            $(".row").addClass("time-block future");
-           
-        }
-        console.log("i: ", i)
-        console.log("Current Time: ", currentTime)
-        console.log("id: ", id)
-        console.log("---------------------------")
     }
-}
 
-$(".saveBtn").click(function (event) {
-    var key = $(this).attr("id");
-    var value = $('task').val();
-    
-    localStorage.setItem(key, value);
+    colorCode();
 });
